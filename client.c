3fd06c72039fd37dfec0d26e6232245ec39dbb96
naivalf27 <naivalf@live.fr>
2017-06-09 13:39:40 +0200

    add init fleuriste

--- client.c
+++ client.c
@@ -4,78 +4,139 @@
 #include <arpa/inet.h> //inet_addr
 #include <unistd.h>
 #include <errno.h>
+#include <ctype.h>
 #include <sys/time.h> //FD_SET, FD_ISSET, FD_ZERO macros
+
+int initClient(char type);
+char typeClient();
+void append(char* s, char c);
  
 int main(int argc , char *argv[]) {
-  int sock;
-  struct sockaddr_in server;
-  char message[1000] , server_reply[2000];
-  fd_set readfds;
-  int max_sd;
-   
-  //Create socket
-  sock = socket(AF_INET , SOCK_STREAM , 0);
-  if (sock == -1) {
-    printf("Could not create socket");
-  }
-  puts("Socket created");
-   
-  server.sin_addr.s_addr = inet_addr("127.0.0.1");
-  server.sin_family = AF_INET;
-  server.sin_port = htons( 8888 );
+	
+	int type = typeClient();
+	
+	return initClient(type);
+}
 
-  //Connect to remote server
-  if (connect(sock , (struct sockaddr *)&server , sizeof(server)) < 0) {
-    perror("connect failed. Error");
-    return 1;
-  }
-   
-  puts("Connected\n");
-   
-  //keep communicating with server
-  while(1) {
-    FD_ZERO(&readfds);
-        //add master socket to set
-    FD_SET(sock, &readfds);
-    FD_SET(fileno(stdin), &readfds);
+char typeClient() {
+	int type = 'p';
+	
+	puts("Qui etes-vous ?");
+	puts("    1. Un client");
+	puts("    2. Un fleuriste");
+	
+	do{
+		type = getchar();
+	}while (type != '1' && type != '2');
+	
+	return type;
+}
 
-    max_sd = (sock > fileno(stdin))?sock:fileno(stdin);
+int initClient(char type){
+	int sock;
+	struct sockaddr_in server;
+	char message[1000] , server_reply[2000];
+	fd_set readfds;
+	int max_sd;
+	
+	//Create socket
+	sock = socket(AF_INET , SOCK_STREAM , 0);
+	if (sock == -1) {
+		printf("Could not create socket");
+	}
+	puts("Socket created");
+	
+	server.sin_addr.s_addr = inet_addr("127.0.0.1");
+	server.sin_family = AF_INET;
+	server.sin_port = htons( 8888 );
+	
+	//Connect to remote server
+	if (connect(sock , (struct sockaddr *)&server , sizeof(server)) < 0) {
+		perror("connect failed. Error");
+		return 1;
+	}
+	
+	puts("Connected\n");
+	
+	char initMessage[128] = "{";
+	append(initMessage, type);
+	append(initMessage, '}');
+	if( write(sock , initMessage , sizeof(initMessage)) < 0) {
+		puts("Init fail");
+		return 1;
+	}
+	
+	int isFleuriste = type == '2';
+	
+	//keep communicating with server
+	while(1) {
+		FD_ZERO(&readfds);
+		//add master socket to set
+		FD_SET(sock, &readfds);
+		FD_SET(fileno(stdin), &readfds);
+		
+		max_sd = (sock > fileno(stdin))?sock:fileno(stdin);
+		
+		memset(message, 0, sizeof(message));
+		memset(server_reply, 0, sizeof(server_reply));
+		//fgets(message,sizeof(message),stdin);
+		
+		int activity = select( max_sd + 1 , &readfds , NULL , NULL , NULL);
+		if ((activity < 0) && (errno!=EINTR))
+		{
+			printf("select error");
+		}
+		
+		if (FD_ISSET(fileno(stdin), &readfds)){
+			if (read(fileno(stdin), message, sizeof(message)) < 0) {
+				puts("fgets failed");
+				break;
+			}
+			if( write(sock , message , sizeof(message)) < 0) {
+				puts("Send failed");
+				break;
+			}
+		}
+		
+		if (FD_ISSET(sock, &readfds)){
+			int result = read(sock, server_reply, sizeof(server_reply));
+			if( result < 0) {
+				puts("recv failed");
+				break;
+			} else if (result == 0){
+				puts("Server disconnected");
+				break;
+			} else {
+				if (isFleuriste == 1){
+					isFleuriste = 0;
+					printf("%s",server_reply);
+					char c;
+					do{
+						c = getchar();
+					}while ( c < '0' || c > '9' );
+					
+					char initMessage[128] = "[";
+					append(initMessage, c);
+					append(initMessage, ']');
+					if( write(sock , initMessage , sizeof(initMessage)) < 0) {
+						puts("Init fail");
+						return 1;
+					}
+					
+				} else {
+					printf("Server reply :       %s\n", server_reply);
+				}
+			}
+		}
+	}
+	
+	close(sock);
+	return 0;
+}
 
-    memset(message, 0, sizeof(message));
-    memset(server_reply, 0, sizeof(server_reply));
-    //fgets(message,sizeof(message),stdin);
-
-    int activity = select( max_sd + 1 , &readfds , NULL , NULL , NULL);
-    if ((activity < 0) && (errno!=EINTR)) 
-    {
-      printf("select error");
-    }
-
-    if (FD_ISSET(fileno(stdin), &readfds)){
-      if (read(fileno(stdin), message, sizeof(message)) < 0) {
-        puts("fgets failed");
-        break;
-      }
-      if( write(sock , message , sizeof(message)) < 0) {
-        puts("Send failed");
-        break;
-      }
-    }
-
-    if (FD_ISSET(sock, &readfds)){
-      int result = read(sock, server_reply, sizeof(server_reply));
-      if( result < 0) {
-        puts("recv failed");
-        break;
-      } else if (result == 0){
-        puts("Server disconnected");
-        break;
-      } else {
-        printf("Server reply :       %s\n", server_reply);
-      }
-    }
-  }
-   
-  close(sock);
-  return 0;
+void append(char* s, char c)
+{
+	int len = strlen(s);
+	s[len] = c;
+	s[len+1] = '\0';
 }
--- server.c
+++ server.c
@@ -9,7 +9,7 @@
 #include <signal.h>
 #include <errno.h>
 #include <sys/time.h> //FD_SET, FD_ISSET, FD_ZERO macros
- 
+
 
 typedef char bool;
 #define true 1
@@ -18,184 +18,270 @@ typedef char bool;
 //the thread function
 void *connection_handler(void *);
 void intHandler(int);
+
+int convClient(int sock, char* message);
+int convFleuriste(int sock, int client);
+int init(int sock, int index,  char* message);
+
 char* bot(const char *);
 bool equal(const char *,const char *);
+void append(char* s, char c);
+char* concat(char* str1, char* str2);
 
 struct thread_args {
-  pthread_t thread;
-  int *sock;
-  int index;
+	pthread_t thread;
+	int *sock;
+	char type;
+	int index;
+	int relier;
 };
 
 struct thread_args* threads;
 int position, socket_desc;
 
 int main(int argc , char *argv[]) {
-  int client_sock , c , *new_sock;
-  struct sockaddr_in server , client;
-
-  signal(SIGINT, intHandler);
-
-  int nbThread = 5; // A remplir avec ton nombre de threads
-  threads = (struct thread_args*)malloc(nbThread*sizeof(struct thread_args));
-  position = 0;
-   
-  //Create socket
-  socket_desc = socket(AF_INET , SOCK_STREAM , 0);
-  if (socket_desc == -1) {
-      printf("Could not create socket");
-  }
-  puts("Socket created");
-   
-  //Prepare the sockaddr_in structure
-  server.sin_family = AF_INET;
-  server.sin_addr.s_addr = INADDR_ANY;
-  server.sin_port = htons( 8888 );
-   
-  //Bind
-  if( bind(socket_desc,(struct sockaddr *)&server , sizeof(server)) < 0) {
-      //print the error message
-      perror("bind failed. Error");
-      return 1;
-  }
-  puts("bind done");
-   
-  //Listen
-  listen(socket_desc , 3);
-   
-  //Accept and incoming connection
-  puts("Waiting for incoming connections...");
-  c = sizeof(struct sockaddr_in);
-  while( (client_sock = accept(socket_desc, (struct sockaddr *)&client, (socklen_t*)&c)) ) {
-    puts("Connection accepted");
-    new_sock = malloc(1);
-    *new_sock = client_sock;
-
-
-
-    if (position < nbThread) {
-      struct thread_args *args = malloc(sizeof *args);
-      if (args != NULL)
-      {
-        args->sock = new_sock;
-        args->index = position;
-
-        if( pthread_create( &(args->thread) , NULL ,  connection_handler , args) < 0) {
-          perror("could not create thread");
-          return 1;
-        }
-        threads[position] = *args;
-        position += 1;
-      }
-    }
-    puts("Handler assigned");
-  }
-   
-  if (client_sock < 0) {
-    perror("accept failed");
-    return 1;
-  }
-   
-  return 0;
+	int client_sock , c , *new_sock;
+	struct sockaddr_in server , client;
+	
+	signal(SIGINT, intHandler);
+	
+	int nbThread = 5; // A remplir avec ton nombre de threads
+	threads = (struct thread_args*)malloc(nbThread*sizeof(struct thread_args));
+	position = 0;
+	
+	//Create socket
+	socket_desc = socket(AF_INET , SOCK_STREAM , 0);
+	if (socket_desc == -1) {
+		printf("Could not create socket");
+	}
+	puts("Socket created");
+	
+	//Prepare the sockaddr_in structure
+	server.sin_family = AF_INET;
+	server.sin_addr.s_addr = INADDR_ANY;
+	server.sin_port = htons( 8888 );
+	
+	//Bind
+	if( bind(socket_desc,(struct sockaddr *)&server , sizeof(server)) < 0) {
+		//print the error message
+		perror("bind failed. Error");
+		return 1;
+	}
+	puts("bind done");
+	
+	//Listen
+	listen(socket_desc , 3);
+	
+	//Accept and incoming connection
+	puts("Waiting for incoming connections...");
+	c = sizeof(struct sockaddr_in);
+	while( (client_sock = accept(socket_desc, (struct sockaddr *)&client, (socklen_t*)&c)) ) {
+		puts("Connection accepted");
+		new_sock = malloc(1);
+		*new_sock = client_sock;
+		
+		
+		
+		if (position < nbThread) {
+			struct thread_args *args = malloc(sizeof *args);
+			if (args != NULL)
+			{
+				args->sock = new_sock;
+				args->index = position;
+				args->type = 'z';
+				args->relier = -1;
+				
+				if( pthread_create( &(args->thread) , NULL ,  connection_handler , args) < 0) {
+					perror("could not create thread");
+					return 1;
+				}
+				threads[position] = *args;
+				position += 1;
+			}
+		}
+		puts("Handler assigned");
+	}
+	
+	if (client_sock < 0) {
+		perror("accept failed");
+		return 1;
+	}
+	
+	return 0;
 }
 
 void intHandler(int dummy) {
-  printf("%s\n", "Ctrl+c");
-    for (int i = 0; i<position; i++) {
-      struct thread_args args = threads[i];
-      pthread_t thread = args.thread;
-      printf( "%p\n", args.thread );
-      int sock = *(int*)args.sock;
-      printf("sock = %d\n", sock);
-      close(sock);
-      pthread_cancel(thread);
-      pthread_join(thread, NULL);
-    }
-    close(socket_desc);
-    free(threads);
-    fflush(stdout);
-    exit(0);
+	printf("%s\n", "Ctrl+c");
+	for (int i = 0; i<position; i++) {
+		struct thread_args args = threads[i];
+		pthread_t thread = args.thread;
+		printf( "%p\n", args.thread );
+		int sock = *(int*)args.sock;
+		printf("sock = %d\n", sock);
+		close(sock);
+		pthread_cancel(thread);
+		pthread_join(thread, NULL);
+	}
+	close(socket_desc);
+	free(threads);
+	fflush(stdout);
+	exit(0);
 }
- 
+
 /*
  * This will handle connection for each client
  * */
 void *connection_handler(void *context) {
-  //Get the socket descriptor
-
-  struct thread_args *args = context;
-
-  int index = args->index;
-  int sock = *(int*)args->sock;
-  printf("sock = %d\n", sock);
-
-  fd_set readfds;
-  int max_sd;
-  char client_message[2000];
-
-  while(true) {
-    FD_ZERO(&readfds);
-        //add master socket to set
-    FD_SET(sock, &readfds);
-    max_sd = sock;
-    memset(client_message, 0, sizeof(client_message));
-    int activity = select( max_sd + 1 , &readfds , NULL , NULL , NULL);
-    if ((activity < 0) && (errno!=EINTR)) 
-    {
-      printf("select error");
-    }
-
-    if (FD_ISSET(sock, &readfds)){
-      int result = read(sock, client_message, sizeof(client_message));
-      if( result < 0) {
-        puts("recv failed");
-        break;
-      } else if (result == 0){
-        puts("Client disconnected");
-        fflush(stdout);
-        break;
-      } else {
-        char *retour_client = bot(client_message);
-        if( write(sock , retour_client , strlen(retour_client)) < 0) {
-          puts("Send failed");
-          break;
-        }
-      }
-    }
-
-  }
-  
-  close(sock);
-  //Free the socket pointer
-  free(args);
-   
-  return 0;
+	//Get the socket descriptor
+	
+	struct thread_args *args = context;
+	
+	int index = args->index;
+	int sock = *(int*)args->sock;
+	
+	fd_set readfds;
+	int max_sd;
+	char client_message[2000];
+	
+	while(true) {
+		FD_ZERO(&readfds);
+		//add master socket to set
+		FD_SET(sock, &readfds);
+		max_sd = sock;
+		memset(client_message, 0, sizeof(client_message));
+		int activity = select( max_sd + 1 , &readfds , NULL , NULL , NULL);
+		if ((activity < 0) && (errno!=EINTR))
+		{
+			printf("select error");
+		}
+		
+		if (FD_ISSET(sock, &readfds)){
+			int result = read(sock, client_message, sizeof(client_message));
+			if( result < 0) {
+				puts("recv failed");
+				break;
+			} else if (result == 0){
+				puts("Client disconnected");
+				fflush(stdout);
+				break;
+			} else {
+				int res = -1;
+				if (threads[index].type == '1'){
+					res = convClient(sock, client_message);
+				} else if (threads[index].type == '2' && threads[index].relier != -1){
+					res = convFleuriste(sock, threads[index].relier);
+				} else {
+					res = init(sock, index, client_message);
+				}
+				if(res == 0){
+					break;
+				}
+			}
+		}
+		
+	}
+	
+	close(sock);
+	//Free the socket pointer
+	free(args);
+	
+	return 0;
 }
 
 char* bot(const char* msg) {
-  char *message;
-  // memset(message, 0, sizeof(message));
-  // strcpy(message, "il faut faire ça");
-  if(equal(msg,"salut ça va ?")){
-    message = "oui et toi ?";
-  } else {
-    message = "il faut faire ça";
-  }
-  return message;
+	char *message;
+	// memset(message, 0, sizeof(message));
+	// strcpy(message, "il faut faire ça");
+	if(equal(msg,"salut ça va ?")){
+		message = "oui et toi ?";
+	} else {
+		message = "il faut faire ça";
+	}
+	return message;
+}
+
+int convClient(int sock, char* message) {
+	char *retour_client = bot(message);
+	if( write(sock , retour_client , strlen(retour_client)) < 0) {
+		puts("Send failed");
+		return 0;
+	}
+	return 1;
+}
+
+int convFleuriste(int sock, int client) {
+	char * retour = "Message fleuriste";
+	if( write(sock , retour , strlen(retour)) < 0) {
+		puts("Send failed");
+		return 0;
+	}
+	return 1;
+}
+
+int init(int sock, int index,  char* message) {
+	if (message[0] == '{' && message[strlen(message)-1] == '}'){
+		threads[index].type = message[1];
+		if (message[1] == '2'){
+			char * retour = "";
+			int count = 1;
+			for (int i = 0; i < position; i++) {
+				if (threads[i].type == '1'){
+					char text[] = "    X. client X\n";
+					text[4] = count+'0';
+					count++;
+					text[14] = i+'0';
+					retour = concat(retour, text);
+				}
+			}
+			if( write(sock , retour , strlen(retour)) < 0) {
+				puts("Send failed");
+				return 0;
+			}
+		} else {
+			char * retour = "Client connected";
+			
+			if( write(sock , retour , strlen(retour)) < 0) {
+				puts("Send failed");
+				return 0;
+			}
+		}
+	} else if (message[0] == '[' && message[strlen(message)-1] == ']'){
+		threads[index].relier = message[1];
+		
+		char * retour = "Fleuriste connected";
+		
+		if( write(sock , retour , strlen(retour)) < 0) {
+			puts("Send failed");
+			return 0;
+		}
+	}
+	return 1;
 }
 
 bool equal(const char *a,const char *b)
 {
-  unsigned len = strlen(b);
-  printf("a = %lu\n", strlen(a));
-  printf("b = %lu\n", strlen(b));
-  if (strlen(a) != (strlen(b)+1))
-    return false;
+	unsigned len = strlen(b);
+	if (strlen(a) != (strlen(b)+1))
+		return false;
+	
+	for (unsigned i = 0; i < len; i += 1)
+	{
+		if (a[i] != b[i])
+			return false; /* strings are not equal */
+	}
+	return true;
+}
 
-  for (unsigned i = 0; i < len; i += 1)
-  {
-    if (a[i] != b[i])
-      return false; /* strings are not equal */
-  }
-  return true;
+void append(char* s, char c)
+{
+	int len = strlen(s);
+	s[len] = c;
+	s[len+1] = '\0';
+}
+
+char* concat(char* str1, char* str2){
+	char * str3 = (char *) malloc(1 + strlen(str1)+ strlen(str2) );
+	strcpy(str3, str1);
+	strcat(str3, str2);
+	return str3;
 }
